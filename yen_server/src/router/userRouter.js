9439696a819a21bdccb7904b07698135024b9443
Quốc Thịnh Trịnh <quth2003@gmail.com>
2024-01-01 01:33:10 +0700

    user back-end

--- yen_server/src/router/userRouter.js
+++ yen_server/src/router/userRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router()
-const userController = require('../controllers/UserController');
-const { authMiddleWare, authUserMiddleWare } = require("../middleware/authMiddleware");
+const userController = require('../controllers/userController');
+const { authMiddleWare, authUserMiddleWare } = require('../middleWare/jwt.middleware');
 
 router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
