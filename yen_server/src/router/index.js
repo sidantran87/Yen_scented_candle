d9ea984d55a276b9a90a64d99891f5963e7b759e
thienhuyuel <thienhuynguyen.uel@gmail.com>
2024-01-01 01:37:50 +0700

    update server

--- yen_server/src/router/index.js
+++ yen_server/src/router/index.js
@@ -1,9 +1,10 @@
 const ProductRouter = require("./productRouter");
-const UserRouter = require("./userRouter")
+// const UserRouter = require("./userRouter")
 
 const routes = (app) => {
+    // app.use('/api/product', ProductRouter)
     app.use('/api/product', ProductRouter)
-    app.use('/api/user', UserRouter)
+    // app.use('/api/user', UserRouter)
 }
 
 module.exports = routes
